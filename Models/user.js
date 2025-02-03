8e0e5657afc2f17a99179a7838381e02be286c84
AniketVerma0614 <aniketverma0614@gmail.com>
2025-02-03 09:12:07 +0530

    Initial commit

---
+++ Models/user.js
@@ -0,0 +1,42 @@
+//user.js
+const mongoose = require('mongoose');
+
+main()
+    .then(() => console.log("Connection successful"))
+    .catch(err => console.log(err));
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/relationDemo');
+}
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+    username: String,
+    addresses: [
+        {   
+            _id: false,
+            location: String,
+            city: String,
+        }
+    ]
+});
+
+const User = mongoose.model("User", userSchema);
+
+const addUsers = async () => {
+    let user1 = new User({
+        username: "sherlockhomes",
+        addresses: [{
+            location: "221B Baker Street",
+            city: "London"
+        }]
+    });
+
+    user1.addresses.push({ location: "P32 WallStreet", city: "London" });
+
+    let result = await user1.save();
+    console.log(result);
+}
+
+addUsers();
